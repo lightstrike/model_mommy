1b80e9238c2d8d7e598415550893d540ea27bc5e
Vanderson Mota <vanderson.mota@gmail.com>
2015-04-06 21:16:51 -0300

    some progress fixing it. It seems you cannot associate unpersisted models

--- test/generic/tests/test_mommy.py
+++ test/generic/tests/test_mommy.py
@@ -318,7 +318,7 @@ class MommyCreatesAssociatedModels(TestCase):
 
     def test_allow_create_fkey_related_model(self):
         try:
-            person = mommy.make(Person, dog_set=[mommy.prepare(Dog), mommy.prepare(Dog)])
+            person = mommy.make(Person, dog_set=[mommy.make(Dog), mommy.make(Dog)])
         except TypeError:
             self.fail('type error raised')
 
